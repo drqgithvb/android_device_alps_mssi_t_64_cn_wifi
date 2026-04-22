f2d5d212277d73a69e2c93a9372c8e1b1faef882
drqgithvb <drqgithub@outlook.com>
2026-04-22 12:57:47 +0800

    enable log in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,3 +110,7 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# enable log
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
